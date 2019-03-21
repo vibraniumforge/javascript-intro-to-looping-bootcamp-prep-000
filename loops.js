c026fc68b397e966b4acf44c3267d14ab29db058
Greg Morgan <gregm9999@gmail.com>
2019-03-21 01:43:35 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,19 @@
+function forLoop(ar) {
+  for (let i = 0; i < 25; i++) {
+    if (i===1) {
+      ar.push("I am 1 strange loop.")
+    } else {
+      ar.push(`I am ${i} strange loops.`)
+    }
+    return ar
+}
+}
+
+function whileLoop(num) {
+  while (num >0) {
+    console.log(num)
+    num--;
+  }
+  console.log("done");
+  return "done";
+}
